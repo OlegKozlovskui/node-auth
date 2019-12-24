c63264d4cc818ab8778ae9c29c884ff0d22db3e1
Oleh Kozlovskyi <okozlovskyi@n-ix.com>
2019-12-24 16:44:42 +0200

    add passport

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = require('express-promise-router')();
+const password = require('password');
+const passportConf = require('../utils/passport');
 
 const { validateBody, schemas } = require('../utils/route-helpers');
 
@@ -11,6 +13,6 @@ const {
 
 router.route('/signin').post(validateBody(schemas.authSchemas), signIn);
 router.route('/signup').post(validateBody(schemas.authSchemas), signUp);
-router.route('/secret').get(getSecret);
+router.route(password.authenticate('jwt', { session: false }), '/secret').get(getSecret);
 
 module.exports = router;
---
+++ utils/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+
+const User = require('../models/User');
+
+// JWT Strategy
+passport.use(new JwtStrategy({
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: process.env.JWT_SECRET
+}, async (payload, done) => {
+  try {
+    const user = await User.findById(payload.id);
+    if (!user) {
+      return done(null, false);
+    }
+
+    done(null, user);
+  } catch (err) {
+    done(err, false)
+  }
+}));
+
+// Local Strategy
+passport.use(new LocalStrategy({
+ usernameField: 'email',
+}, async (email, password, done) => {
+  try {
+    const user = await User.findOne(email);
+    if (!user) {
+      return done(null, false);
+    }
+
+    done(null, user);
+  } catch (err) {
+    done(err, false)
+  }
+}));
